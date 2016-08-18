5c05d434650d127b93f04a584dccb14ab4028229
Peyton Duncan <peyton.duncan45@gmail.com>
2016-08-18 11:19:31 -0400

    Added a Countdown to each coin

--- src/com/peyton/coinflipstats/Coin.java
+++ src/com/peyton/coinflipstats/Coin.java
@@ -3,21 +3,34 @@ package com.peyton.coinflipstats;
 import java.util.Random;
 
 public class Coin {
+	
+	
 	//Fields
 	Random random = new Random();
+	String facing = "";
+	
+	
 	//Constructor
 	public Coin(){}
 	
+	
+	
 	//Methods
+	
+	//Returns the result of a flip based on 50/50 odds
 	public String flip(){
 		String flipResult;
-		int randomint = random.nextInt(2);
+		int randomint = random.nextInt(2); //Returns a random number, either 0 or 1 because the paramer of random.nextInt is an integer in the range of [0,n)
 		if(randomint == 0){
 			flipResult = "Tails";
 		}
 		else{
 			flipResult = "Heads";
 		}
+		//Shorthand for the previous conditional using ternary:
+		//flipResult=(randomint==0)?"Tails":"Heads";
+		
+		facing = flipResult;
 		return flipResult;
 	}
 }
